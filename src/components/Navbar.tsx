966d6096055d108762e41d9c7d6430e1519ad054
mrbridge-dev <oseijuantuah@gmail.com>
2023-01-03 00:14:16 +0000

    new branch

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 100vw;
+    height: 80px;
+    display: flex;
+    padding-left: 80px;
+    padding-right: 80px;
+    flex-flow: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--color-elements);
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.0562443);
+`;
+
+const Title = styled.h2`
+`;
+
+const Mode = styled.span``;
+
+const Navbar:React.FC = () => {
+  return (
+    <Container>
+        <Title>Where in the world</Title>
+        <Mode>Dark mode</Mode>
+    </Container>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+
+const Container = styled.div``;
+
+const Homepage:React.FC = () => {
+  return (
+    <Container>
+        <Navbar />
+        Homepage
+    </Container>
+  )
+}
+
+export default Homepage
